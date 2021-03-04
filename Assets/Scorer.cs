3c9849602aecea3a5415e84394f91b7bc00c169e
bloges56 <brady.drew.logan@gmail.com>
2021-03-04 16:44:01 -0600

    added a dropping block

---
+++ Assets/Scorer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    private int score = 0;
+    private void OnCollisionEnter(Collision other) 
+    {
+        if( other.gameObject.name != "Plane")
+        {
+            score += 1;
+            Debug.Log($"You've bumped into a thing this many times: {score}");
+        }
+    }
+}
