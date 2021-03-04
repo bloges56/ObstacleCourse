f7164c415215b170d00fca19b90f243664c4c605
bloges56 <brady.drew.logan@gmail.com>
2021-03-04 11:56:26 -0600

    refactored code and added logs

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -8,11 +8,23 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstuctions();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstuctions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Use WASD to move");
+        Debug.Log("Avoid the obstacles!");
+    }
+
+    void MovePlayer()
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime *  moveSpeed;
